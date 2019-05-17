63d6b05526eb85558eda490af2c4ce25f371deba
brucbr <44393338+brucbr@users.noreply.github.com>
2019-05-17 22:28:59 +1000

    Changed to reflect servo_party.py changes

--- stop_servos.py
+++ stop_servos.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from pyax12.connection import Connection
-import pyax12.packet as pk
-import serial
+from servo_party import ServoParty
 
-sc = Connection(port="/dev/ttyACM0", baudrate=1000000)
+sp = ServoParty()
 
-sc.set_speed(1, 0)
-sc.set_speed(2, 0)
-sc.set_speed(3, 0)
-sc.set_speed(4, 0)
-sc.close()
+sp.stop()
+sp.close()
