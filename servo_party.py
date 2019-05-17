1d8e75a093bc8fa19bceb70681dd298b692d320a
brucbr <44393338+brucbr@users.noreply.github.com>
2019-05-17 22:24:27 +1000

    PyPot changes
    This marks the shift from Pyax-12 to PyPot. PyPot seems to be significantly better to pyax-12 due to its larger motor support range and robot configuration possibilities

--- servo_party.py
+++ servo_party.py
@@ -1,69 +1,19 @@
-#!/usr/bin/env python3
-from pyax12.connection import Connection
-from enum import IntEnum
+from pypot.robot import from_json
+import time
 
-
-class Servo(IntEnum):
-    LEFT_FRONT = 1
-    RIGHT_FRONT = 2
-    LEFT_BACK = 3
-    RIGHT_BACK = 4
-
-
-class ServoParty:
-    def __init__(self):
-        self.sc_dynamixel = Connection(port="/dev/ttyACM0", baudrate=1000000)
-        self.speed_factor = 512
-        self.last_left = 0
-        self.last_right = 0
-
-    def stop(self):
-		# Set all servos to 0
-        self.sc_dynamixel.set_speed(1, 0)
-        self.sc_dynamixel.set_speed(2, 0)
-        self.sc_dynamixel.set_speed(3, 0)
-        self.sc_dynamixel.set_speed(4, 0)
-		# Close the connection
-        self.sc_dynamixel.close()
-	
-    def setup_servo(self, dynamixel_id):
-        # Set the "wheel mode"
-        self.sc_dynamixel.set_cw_angle_limit(dynamixel_id, 0, degrees=False)
-        self.sc_dynamixel.set_ccw_angle_limit(dynamixel_id, 0, degrees=False)
-        
-    def move_raw(self, left, right):
-        # Left side
-        self.sc_dynamixel.set_speed(Servo.LEFT_FRONT, left)
-        self.sc_dynamixel.set_speed(Servo.LEFT_BACK, left)
-        # Right side
-        self.sc_dynamixel.set_speed(Servo.RIGHT_FRONT, right)
-        self.sc_dynamixel.set_speed(Servo.RIGHT_BACK, right)
-    
-    def move_raw_left(self, left):
-        # Left side
-        self.sc_dynamixel.set_speed(Servo.LEFT_FRONT, left)
-        self.sc_dynamixel.set_speed(Servo.LEFT_BACK, left)
-        
-    def move_raw_right(self, right):
-        # Right side
-        self.sc_dynamixel.set_speed(Servo.RIGHT_FRONT, right)
-        self.sc_dynamixel.set_speed(Servo.RIGHT_BACK, right)
-
-    def move(self, left, right):
-
-        # Different motors need to spin in different directions. We account for that here.  
-        if (left < 0):
-            left *= -1
-            left += 1024
-        if (right < 0):
-            right *= -1
-        elif right < 1024:
-            right += 1024
-        
-        # Only send message if it's different to the last one
-        if (left != self.last_left and right != self.last_right):
-            self.move_raw(left, right)
-        
-        # Store this message for comparison next time
-        self.last_left = left
-        self.last_right = right
+class ServoParty():
+	def __init__(self):
+		self.robot = from_json('my_robot.json')
+		for m in self.robot.motors: # Note that we always provide an alias for all motors.
+			m.compliant = False
+			m.goal_position = 0
+	def move(self, left, right):
+		self.robot.front_left.moving_speed = left;
+		self.robot.back_left.moving_speed = left;
+		self.robot.front_right.moving_speed = right;
+		self.robot.back_right.moving_speed = right;
+	def stop(self):
+		self.robot.front_left.moving_speed = 0;
+		self.robot.back_left.moving_speed = 0;
+		self.robot.front_right.moving_speed = 0;
+		self.robot.back_right.moving_speed = 0;
